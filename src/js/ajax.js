32a5335029f56eba3a53fc7dd04f3e310c481c31
Celso Bastos <celsorico@gmail.com>
2022-07-14 22:29:10 -0300

    ajax

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,14 +1,27 @@
 "use strict"
+// function loadXMLDoc() {
+//     let xmlhttp = new XMLHttpRequest();
+//     xmlhttp.onreadystatechange = function () {
+//         console.log('test 5');
+//         let resposta = this.responseText;
+//         if (this.status == 200) {
+//             let resposta = this.responseText;
+//             console.log(resposta);
+//         }
+//     }
+//     xmlhttp.open("GET", "http://viacep.com.br/ws/01312-001/json/", true);
+//     xmlhttp.send;
+// }
 
-let xmlhttp = new XMLHttpRequest();
-
-xmlhttp.onreadystatechange = function () {
-
-    if (this.readyState === 4 && this.status === 200) {
-        let resposta = this.responseText;
-        console.log(resposta);
-    }
+function loadXMLDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        //console.log(JSON.parse(this.responseText));
+      }
+    };
+    xhttp.open("GET", "http://127.0.0.1:5501/src/content-ajax/content.txt", true);
+    xhttp.send();
 }
-
-xmlhttp.open("GET", 'http://127.0.0.1:5501/src/content-ajax/content.txt', true);
-xmlhttp.send;
+loadXMLDoc();
